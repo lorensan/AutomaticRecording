2801d26fb2075bcd4631681d53c0f6852e6be7d5
lorenzo <lorensan90@gmail.com>
2024-01-17 10:30:53 +0100

    Code Refactor and using english as main language

--- obsLauncher.bat
+++ obsLauncher.bat
@@ -4,61 +4,59 @@ setlocal enabledelayedexpansion
 
 
 
-rem Variables Locales
+rem Local Variables
 set "recordFolder=C:\test"
 set "obsFolder=C:\Program Files\obs-studio\bin\64bit"
-set "max_archivos=10"
-set "archivo_mas_antiguo="
-set "fecha_mas_antigua="
+set "max_files=10"
+set "file_older="
+set "date_older="
 set "counter=1"
 
 cd %recordFolder%
 
-rem Contamos el número de archivos que hay en la carpeta para saber si tenemos que borrar
+rem We count the number of files that exist in the folder to know if we have to remove the older
 for %%i in (*) do (
     set /a "counter+=1"
 )
 
-echo Numero de archivos: !counter!
+echo Number of files: !counter!
 
-rem Solo borramos el más antiguo si el numero de archivos es mayor a 10
-if !counter! gtr !max_archivos! (
+rem We remove only the older file modified when is more than 10 files (defined in local variables)
+if !counter! gtr !max_files! (
 
     for %%i in (*) do (
-        set "archivo_actual=%%i"
+        set "main_file=%%i"
         
-        rem Obtener la fecha de última modificación del archivo, usamos esta forma de parseo por que no funciona obtenerla directamente con 'dir'
-        set "fecha_actual="
+        rem We get the date of the last file modification, we use this parsing technic  because it doesn't work how to get directly using 'dir' command.
+        set "main_date="
         for /f "tokens=1-3 delims= " %%j in ('dir /tw "%%i" ^| findstr /b "^[0-9]"') do (
             set "dia=%%j"
             set "mes=%%k"
             set "anio=%%l"
-            set "fecha_actual=!dia!!mes!!anio!"
+            set "main_date=!dia!!mes!!anio!"
         )
 
-        if not defined fecha_mas_antigua (
-            set "fecha_mas_antigua=!fecha_actual!"
-            set "archivo_mas_antiguo=!archivo_actual!"
-            rem echo !counter! Guardada la primera fecha: !archivo_actual! - Fecha: !fecha_actual!
+        if not defined date_older (
+            set "date_older=!main_date!"
+            set "file_older=!main_file!"
+            rem echo [DEBUG]  !counter! Saved the first date: !main_file! - Fecha: !main_date!
         )else (
-            if !fecha_actual! lss !fecha_mas_antigua! (
+            if !main_date! lss !date_older! (
                 
-                echo Borrando el mas antiguo !archivo_actual! - Fecha: !fecha_actual!
+                echo Removing the older file !main_file! - Fecha: !main_date!
             )
-            set "fecha_mas_antigua=!fecha_actual!"
-            set "archivo_mas_antiguo=!archivo_actual!"
-            rem echo Nuevo Archivo !archivo_actual! - Fecha: !fecha_actual!
-            
+            set "date_older=!main_date!"
+            set "file_older=!main_file!"
+            rem echo [DEBUG] New file !main_file! - Fecha: !main_date!
         )
-        rem echo !counter! Archivo: !archivo_actual! - Fecha: !fecha_actual!
+        rem echo [DEBUG]  !counter! Archivo: !main_file! - Fecha: !main_date!
     )
 
-echo Borrando... !archivo_mas_antiguo! - Fecha: !fecha_mas_antigua!
-rem del !archivo_actual!
+echo Borrando... !file_older! - Fecha: !date_older!
+del !main_file!
 )
 
-rem Iniciamos OBS Studio minimizado en la barra de tareas
-
+rem We start OBS Studio minimizing to background
 cd %obsFolder%
 
 start obs64.exe --startrecording
@@ -66,7 +64,8 @@ start obs64.exe --startrecording
 timeout /t 5 /nobreak > nul
 nircmd win hide process obs64.exe
 
-:: start "" "C:\Program Files\Notepad++\notepad++.exe"
+rem We start the final program that we would like to execute
+start "" "C:\Program Files\Notepad++\notepad++.exe"
 
 
 endlocal
